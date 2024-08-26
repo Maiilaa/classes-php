b8a78fd5cfd0d92823939a77c122d600c1705a0a
Maiilaa <zirarimaila8@gmail.com>
2024-08-26 11:58:25 +0200

    debut de connexion à la base de donnée

--- User.php
+++ User.php
@@ -6,13 +6,15 @@ class User{
     public $firstname;
     public $lastname;
     private $conn;
-    
+
     public function __construct($id, $login, $email, $firstname, $lastname){
-        $this->id = $id;
-        $this->login = $login;
-        $this->email = $email;
-        $this->firstname = $firstname;
-        $this->lastname = $lastname;
+        $this->conn = new mysqli($localhost, $user, $password, $database);
+        $this->conn = new mysqli('localhost', 'root', 'password123', 'classes');
+
+        if ($this->conn->connect_error) {
+            die("Erreur de connexion à la base de données : " . $this->conn->connect_error);
+        }
     }
+        
 }
 ?>
