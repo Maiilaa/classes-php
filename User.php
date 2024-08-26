66eba1b113240d38f4ad49392334a7129cbf39de
Maiilaa <zirarimaila8@gmail.com>
2024-08-26 11:22:41 +0200

    yess sir

--- User.php
+++ User.php
@@ -5,6 +5,14 @@ class User{
     public $email;
     public $firstname;
     public $lastname;
+    private $conn;
     
-    blablabla
+    public function __construct($id, $login, $email, $firstname, $lastname){
+        $this->id = $id;
+        $this->login = $login;
+        $this->email = $email;
+        $this->firstname = $firstname;
+        $this->lastname = $lastname;
+    }
 }
+?>
